f39fd5a6cfd8fabe0df098bca2c71f6e0b9a8a4e
worldmaker <worldmaker18349276@gmail.com>
2023-01-30 23:00:53 +0800

    add input trainer

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use btleplug::api::{Central, Manager, ScanFilter};
 use btleplug::platform;
 use cube::format_moves;
 use std::error::Error;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 use bluetooth::gancubev2::{GanCubeV2Builder, ResponseMessage};
 
@@ -42,7 +44,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
     println!("{}", DEFAULT_CHEATSHEET);
     println!();
 
-    let mut input_handler = CuboardInputPrinter::new();
+    // let mut input_handler = CuboardInputPrinter::new(CuboardInput::new());
+    let text = BufReader::new(File::open("test.txt")?)
+        .lines()
+        .map(|l| l.unwrap());
+    let mut input_handler = CuboardInputTrainer::<_, 3>::new(CuboardInput::new(), text);
     let handle = gancube
         .register_handler(Box::new(move |msg| input_handler.handle_message(msg)))
         .await?;
@@ -128,8 +134,8 @@ struct CuboardInputPrinter {
 }
 
 impl CuboardInputPrinter {
-    fn new() -> Self {
-        CuboardInputPrinter { input: CuboardInput::new() }
+    fn new(input: CuboardInput) -> Self {
+        CuboardInputPrinter { input }
     }
 
     fn handle_message(&mut self, msg: ResponseMessage) {
@@ -179,6 +185,108 @@ impl CuboardInputPrinter {
                 CREL,
                 self.input.complete_part(),
                 self.input.remain_part()
+            );
+            let _ = std::io::Write::flush(&mut std::io::stdout());
+        }
+    }
+}
+
+struct CuboardInputTrainer<T: Iterator<Item = String>, const N: usize> {
+    input: CuboardInput,
+    text: T,
+    lines: [String; N],
+}
+
+impl<T: Iterator<Item = String>, const N: usize> CuboardInputTrainer<T, N> {
+    fn new(input: CuboardInput, mut text: T) -> Self {
+        let mut lines = Vec::new();
+        for _ in 0..N {
+            lines.push(text.next().unwrap_or_default())
+        }
+        let lines = lines.try_into().unwrap();
+        CuboardInputTrainer { input, text, lines }
+    }
+
+    fn handle_message(&mut self, msg: ResponseMessage) {
+        if self.input.count.is_none() {
+            if let ResponseMessage::State { count, state: _ } = msg {
+                self.input.count = Some(count);
+
+                let cursor = self.lines[0].chars().next().unwrap_or(' ');
+                print!("\x1b[2m{}\x1b[m", self.lines[0]);
+                print!("\r\x1b[7m{}\x1b[m\n", cursor);
+                for line in self.lines.iter().skip(1) {
+                    println!("\x1b[2m{}\x1b[m", line);
+                }
+                print!("\r\x1b[100m\x1b[2K \x1b[m\r");
+                let _ = std::io::Write::flush(&mut std::io::stdout());
+            }
+
+            return;
+        }
+
+        if let ResponseMessage::Moves {
+            count,
+            moves,
+            times: _,
+        } = msg
+        {
+            let curr_count = self.input.count.unwrap();
+            let diff = {
+                let delta = count.wrapping_add(curr_count.wrapping_neg());
+                let delta_ = delta.wrapping_neg();
+                delta.min(delta_) as usize
+            };
+            self.input.count = Some(count);
+
+            for &mv in moves[..diff].iter().rev() {
+                if let Some(mv) = mv {
+                    self.input.cuboard.input(mv);
+                }
+            }
+
+            print!("\x1b[{}A", N);
+            for line in self.lines.iter() {
+                println!("\r\x1b[2m\x1b[2K{}\x1b[m", line);
+            }
+
+            let text = self.input.complete_part();
+            let decoreated_text: String = text
+                .trim_end_matches('\n')
+                .chars()
+                .zip(self.lines[0].chars().chain([' '].into_iter().cycle()))
+                .map(|(a, b)| {
+                    if a == b {
+                        format!("{}", a)
+                    } else {
+                        format!("\x1b[41m{}\x1b[m", a)
+                    }
+                })
+                .collect();
+
+            print!("\x1b[{}A", N);
+            if text.contains('\n') {
+                let cursor = self.lines[1].chars().next().unwrap_or(' ');
+                print!("\r{}\n\x1b[7m{}\x1b[m", decoreated_text, cursor);
+            } else {
+                let cursor = self.lines[0].chars().nth(text.len()).unwrap_or(' ');
+                print!("\r{}\x1b[7m{}\x1b[m\n", decoreated_text, cursor);
+            }
+            print!("\x1b[{}B\r", N - 1);
+
+            if text.contains('\n') {
+                assert!(!text[..text.len() - 1].contains('\n'));
+                let new_line = self.text.next().unwrap_or_default();
+                print!("\r\x1b[m\x1b[2K\r\x1b[2m{}\x1b[m\n", new_line);
+                self.input.cuboard.finish();
+                self.lines.rotate_left(1);
+                self.lines[N - 1] = new_line;
+            }
+
+            print!(
+                "\r\x1b[100m\x1b[2K\x1b[4m{}\x1b[2m{}\x1b[m\r",
+                self.input.complete_part(),
+                self.input.remain_part(),
             );
             let _ = std::io::Write::flush(&mut std::io::stdout());
         }
